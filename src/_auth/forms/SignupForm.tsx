8f9d79ef1859f81baf84ea75bc118e80f6fb3eb7
swastideep maharana <swastideep67maharana@gmail.com>
2024-06-18 00:02:48 +0530

    #added-tsconfig-file

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -1,8 +1,14 @@
+import { Button } from "@/components/ui/button"
+
+
 import React from 'react'
 
 const SignupForm = () => {
   return (
-    <div>SignupForm</div>
+    <div>
+        <Button>Click me</Button>
+      </div>
+
   )
 }
 
